c87142e48a49cbd1e1d461a4375ab6d94d423dfe
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2019-09-15 00:32:44 -0400

    Minor Changes

--- parser/parser.js
+++ parser/parser.js
@@ -108,33 +108,3 @@ let sampleTags = ["biKe", "HELLO", "cAR", "motorcycle", "TRUck", "yeeet"];
 
 console.log(vehicleClassifier());
 
-//Second Sentence Parsing 
-function secondSentence() {
-
-    let seconStartStrings = ["Further, ", "Additionally "]
-
-    for(let tag in sampleTags) {
-
-    
-    
-        let curTag = sampleTags[tag];
-        curTag = curTag.toLowerCase();
-    
-        if(cov.vehicles[curTag] == undefined) {
-    
-            //Skip Block
-    
-        }
-    
-        else {
-    
-            curTags.push(curTag);
-            vehicleCount++;
-    
-        }
-    
-    }
-
-}
-
-
