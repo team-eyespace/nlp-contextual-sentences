01684c6935aef147b50244d4a9b8cddb0e40c142
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2019-12-07 17:57:46 -0500

    dart parser

---
+++ parser-dart/parser.dart
@@ -0,0 +1,18 @@
+class DartParser {
+
+var startStrings = ["There is ", "The app sees ", "You are looking at "];
+
+var endStrings = [" near you.", " in front of you.", " alongside you."];
+
+
+var sampleTags = ["caR", "baby", "man", "woman", "bottle"];
+
+
+
+}
+void main() {
+
+
+print("hello world");
+
+}
